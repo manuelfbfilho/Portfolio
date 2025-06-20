1c26b92fc5570b7bf77c6e91d8d2def1bfe78959
Manuel Fernandes <151965418+manuelfbfilho@users.noreply.github.com>
2025-06-20 09:50:47 -0300

    Remove as poastas Loterias e rpa da pasta 1-Python do repositório.

--- 1-Python/rpa/extraindo_imagens02.py
+++
@@ -1,8 +0,0 @@
-import pypdf
-
-leitor = pypdf.PdfReader("ambev.pdf")
-
-for pagina in leitor.pages:
-    for imagem in pagina.images:
-        with open(f'imagens/{imagem.name}', 'wb') as file:
-            file.write(imagem.data)
--- 1-Python/rpa/extraindo_texto.py
+++
@@ -1,9 +0,0 @@
-import pypdf
-
-leitor = pypdf.PdfReader("ambev.pdf")
-
-textos = [pagina.extract_text() for pagina in leitor.pages]
-texto = ''.join(textos)
-
-print(leitor.pages[0].extract_text())
-print(texto)
